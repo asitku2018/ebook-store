b273a6d258d67f8618d552aace9fe866021a974b
asitku2018 <asitku2018@gmail.com>
2026-07-18 17:55:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Views/**/*.php",
+    "./frontend/src/**/*.ts"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
